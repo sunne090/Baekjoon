26696edde0497dbdc77e84437bcb913e848b4663
sunne090 <87286554+sunne090@users.noreply.github.com>
2023-01-08 02:25:36 +0900

    [Bronze IV] Title: 기찍 N, Time: 1068 ms, Memory: 32576 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 0; i < N; i++){
+            System.out.println(N-i);
+        }
+    }
+}
