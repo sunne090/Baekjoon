f745cc0f639f12fe5521b638f5d9340c9d3067ab
sunne090 <87286554+sunne090@users.noreply.github.com>
2023-01-23 01:56:56 +0900

    [Silver V] Title: 소수 찾기, Time: 216 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 0;
+        for(int i = 0; i < N; i++) {
+            boolean isPrime = true;
+            int num = sc.nextInt();
+            if(num == 1) {
+                continue;
+            }
+            for(int j = 2; j <= Math.sqrt(num); j++) {
+                if(num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
