bc0094388fb3bdc87c83428153418fc4bd6e1d1c
sunne090 <87286554+sunne090@users.noreply.github.com>
2023-02-17 11:03:19 +0900

    Update 배열의 유사도.java

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         for(int i = 0; i < s1.length; i++) {
         	for(int j = 0; j < s2.length; j++) {
-        		if(s1[i].equals( s2[j])) { //문자열을 비교할 때는 == 대신 equals를 
+        		if(s1[i].equals( s2[j])) { //문자열을 비교할 때 equals를 사용하기
         			answer = answer + 1;
         			break;
         		}
