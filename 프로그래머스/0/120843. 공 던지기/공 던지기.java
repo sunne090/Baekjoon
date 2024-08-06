4489411af75abf68b95c9d45b523d6a68b2b7d23
sunne090 <87286554+sunne090@users.noreply.github.com>
2024-08-06 14:36:06 +0900

    Update 공 던지기.java

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] numbers, int k) {
         int answer = 1;
-        for(int i = 1; i <= k-1; i++){
+        for(int i = 1; i <= k-1; i++){ //공을 던질 사람의 번호를 구하는 것이므로 k-1 필요(공을 받는 사람이면 k로 해야됨)
             answer = answer + 2;
             if(answer > numbers[numbers.length-1]){
                 answer = answer - numbers.length;
