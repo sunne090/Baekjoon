4e238e756a1a66f62e8318cb87de116ae2bb4a8b
sunne090 <87286554+sunne090@users.noreply.github.com>
2025-02-28 13:39:04 +0900

    [level 0] Title: 약수 구하기, Time: 2.71 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,24 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
-    public int[] solution(int n) {
-        int count = 0;
+    //어떤 수 n의 약수 (a, b)는 항상 a * b = n을 만족.
+    //즉, 한쪽 약수가 √N 이하이면, 다른 쪽 약수는 √N 이상이므로 반만 검사하면 됨!
+    public static int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
 
-        // 약수 개수 세기
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i * i <= n; i++) {
             if (n % i == 0) {
-                count++;
+                list.add(i);
+                if (i != n / i) {
+                    list.add(n / i);
+                }
             }
         }
 
-        int[] answer = new int[count];
-        int index = 0;
-
-        // 약수 저장
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                answer[index++] = i;
-            }
-        }
-
-    return answer;
+        list.sort(null);  // 오름차순 정렬
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
