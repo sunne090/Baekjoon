fa139076628296904e362b549ad136444c63e441
sunne090 <87286554+sunne090@users.noreply.github.com>
2025-03-19 17:44:14 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.45 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,16 +2,8 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String[] my_string_array = new String[my_string.length()];
-        for (int i = 0; i < my_string.length(); i++) {
-            my_string_array[i] = my_string.toLowerCase().substring(i, i + 1);
-            System.out.println(my_string_array[i]);
-        }
-        Arrays.sort(my_string_array);
-        for(int i = 0; i < my_string_array.length; i++) {
-            answer+= my_string_array[i];
-        }
-        return answer;
+        char[] my_chars = my_string.toLowerCase().toCharArray();
+        Arrays.sort(my_chars);
+        return new String(my_chars);
     }
 }
